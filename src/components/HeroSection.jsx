d6d774bfce93d2fe17f980a68ae59ad23c3bbc69
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2025-04-19 18:14:26 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,14 +20,15 @@ const HeroSection = () => {
         <div className="w-full lg:w-1/2 flex items-center justify-center bg-white p-6">
           <div className="text-center lg:text-left">
             <h2 className="text-3xl lg:text-4xl font-bold text-gray-800">
-            Empower Your ,
+              Empower Your ,
               <span className="hidden md:inline">
                 <br />
               </span>{" "}
-             Finances with Peer-to-Peer Contribution Cycles
+              Finances with Peer-to-Peer Contribution Cycles
             </h2>
             <p className="mt-4 text-lg text-gray-400">
-            Experience the benefits of a community-driven support network built on trust, sustainability, and transparency.
+              Experience the benefits of a community-driven support network
+              built on trust, sustainability, and transparency.
             </p>
 
             <div className="flex flex-row gap-4 mt-8">
@@ -40,38 +41,40 @@ const HeroSection = () => {
             </div>
 
             {/* Images and Text Section */}
-            <div className="flex flex-row gap-2 mt-8 justify-start">
+            <div className="flex flex-row mt-8 items-start gap-2 sm:gap-4 flex-wrap">
               {/* Images Section */}
-              <div className="flex w-1/3">
+              <div className="flex w-auto">
                 <Image
                   src="/images/Ellipse14.svg"
                   alt="Hero"
-                  width={60}
-                  height={60}
+                  width={50}
+                  height={50}
                   className="rounded-lg object-contain max-w-full h-auto"
                 />
                 <Image
                   src="/images/Ellipse15.svg"
                   alt="Hero"
-                  width={60}
-                  height={60}
+                  width={50}
+                  height={50}
                   className="-ml-3 rounded-lg object-contain max-w-full h-auto"
                 />
                 <Image
                   src="/images/Ellipse16.svg"
                   alt="Hero"
-                  width={60}
-                  height={60}
+                  width={50}
+                  height={50}
                   className="-ml-3 rounded-lg object-contain max-w-full h-auto"
                 />
               </div>
+
               {/* Text Section */}
-              <div className="w-2/3">
-                <h2 className="text-3xl text-black flex items-center">
-                  <RiArrowUpSFill className="mr-2" /> 15 Million+
+              <div className="flex-1 min-w-0">
+                <h2 className="text-sm sm:text-xl md:text-3xl text-black flex items-center">
+                  <RiArrowUpSFill className="mr-1 sm:mr-2" /> 15 Million+
                 </h2>
-                <p className="mt-4 text-[15px] text-gray-400">
-                Supported transactions in peer-to-peer contribution cycles from communities around the world.
+                <p className="mt-1 sm:mt-3 text-xs sm:text-sm text-gray-400">
+                  Supported transactions in peer-to-peer contribution cycles
+                  from communities around the world.
                 </p>
               </div>
             </div>
