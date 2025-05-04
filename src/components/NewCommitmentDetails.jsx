04e0d1842f8d88f808e4a22b3a3d531a9f0239e3
mintqlick <mintqlick@gmail.com>
2025-05-04 18:13:22 +0100

    adjust time

--- src/components/NewCommitmentDetails.jsx
+++ src/components/NewCommitmentDetails.jsx
@@ -110,9 +110,8 @@ export default function NewCommitmentDetails({
 }
 
 function formatCountdown(seconds) {
-  const days = Math.floor(seconds / (3600 * 24));
   const hours = Math.floor((seconds % (3600 * 24)) / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const secs = Math.floor(seconds % 60);
-  return `${days}d ${hours}h ${minutes}m ${secs}s`;
+  return `${hours}h ${minutes}m ${secs}s`;
 }
