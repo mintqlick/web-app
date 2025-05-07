4fdbaed3bb760b47fe21cb46398000dbb47531c3
Mintqlick <mintqlick@gmail.com>
2025-05-07 14:14:14 +0100

    fixing

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,24 @@
-import { updateSession } from '@/utils/supabase/middleware';
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request) {
   return await updateSession(request);
 }
 
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for the ones starting with:
-//      * - _next/static (static files)
-//      * - _next/image (image optimization files)
-//      * - favicon.ico (favicon file)
-//      * Feel free to modify this pattern to include more paths.
-//      */
-//     "/", // Protect home page
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     "/(api|trpc)(.*)", // Protect all API routes as well
-//   ],
-// };
-///((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|^/$).*)
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|callback).*)'],
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    // "/", // Protect home page
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // "/(api|trpc)(.*)", // Protect all API routes as well
+  ],
+};
+///((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|^/$).*)
+// export const config = {
+//   matcher: ['/((?!_next/static|_next/image|favicon.ico|callback).*)'],
+// }
